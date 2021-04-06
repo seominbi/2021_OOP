50c3fdc841869b7d8649d3fcbd16357a1718a84c
seominbi <zyuqw1193@naver.com>
2021-04-06 16:23:51 +0900

    210406:SOLUTION#1

---
+++ src/OOP1_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class OOP1_2 {
+    public static void main(String[] args){
+        System.out.print("2자리 정수 입력(10~99)>>");
+        Scanner scanner = new Scanner(System.in);
+        int num=scanner.nextInt();
+        if(num/10==num%10){
+            System.out.println("Yes! 10의 자리와 1의 자리가 같습니다.");
+        }
+    }
+}
---
+++ src/first/Hello3040.java
@@ -0,0 +1,7 @@
+package first;
+
+public class Hello3040 {
+    public static void main(String[] args) {
+        System.out.println("3th");
+    }
+}
